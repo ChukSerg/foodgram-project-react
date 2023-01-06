41f5c84d1f1f217f9f5da6dbe67db276d928eac9
Sergey Chukin <chuk2208@yandex.ru>
2023-01-06 19:54:59 +0500

    Деплой

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,11 +1,28 @@
-from django_filters import rest_framework as drf
-from recipes.models import Ingredient
+from django.contrib.auth import get_user_model
+import django_filters
+
+from recipes.models import Ingredient, Recipes
+
+User = get_user_model()
 
 
-class IngredientFilter(drf.FilterSet):
-    """Фильтр ингредиентов"""
-    name = drf.CharFilter(field_name='name', lookup_expr='istartswith')
+class IngredientFilter(django_filters.FilterSet):
+    name = django_filters.CharFilter(
+        field_name='name',
+        lookup_expr='istartswith'
+    )
 
     class Meta:
         model = Ingredient
         fields = ('name',)
+
+
+class RecipeFilter(django_filters.FilterSet):
+    tags = django_filters.AllValuesMultipleFilter(field_name='tags__slug')
+    author = django_filters.ModelChoiceFilter(
+        queryset=User.objects.all()
+    )
+
+    class Meta:
+        model = Recipes
+        fields = ('tags', 'author',)
