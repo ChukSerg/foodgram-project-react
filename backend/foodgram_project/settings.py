20c926e0ecbdab26bc638e88566fd541343c16c6
Sergey Chukin <chuk2208@yandex.ru>
2023-01-06 13:43:23 +0500

    Деплой

--- backend/foodgram_project/settings.py
+++ backend/foodgram_project/settings.py
@@ -141,6 +141,5 @@ REST_FRAMEWORK = {
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
     ],
 }
